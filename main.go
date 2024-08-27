767a273c31b0664fb3f5618224bedd3c10dbc09b
claudealdric <cvaldric@gmail.com>
2024-08-26 20:53:07 -0400

    Add a log when starting the server

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ const port = 8080
 
 func main() {
 	server := NewServer()
+	log.Printf("Starting server on port %d", port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), server)
 	log.Fatal(err)
 }
