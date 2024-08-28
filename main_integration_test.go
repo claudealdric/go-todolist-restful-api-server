a9f69e31e8be69dc5c636a2f704e0f04ff8d09b0
claudealdric <cvaldric@gmail.com>
2024-08-27 22:24:22 -0400

    Fail the test immediately when encountering a request error

--- main_integration_test.go
+++ main_integration_test.go
@@ -14,7 +14,7 @@ func TestServer(t *testing.T) {
 	t.Run("responds with a 200 OK status on the root path", func(t *testing.T) {
 		request, err := http.NewRequest(http.MethodGet, "/", nil)
 		if err != nil {
-			t.Errorf("an error occurred during the request: %v", err)
+			t.Fatalf("an unexpected error occurred: %v", err)
 		}
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
@@ -24,7 +24,7 @@ func TestServer(t *testing.T) {
 	t.Run("responds with a 404 not found status on the root path", func(t *testing.T) {
 		request, err := http.NewRequest(http.MethodGet, "/not-found", nil)
 		if err != nil {
-			t.Errorf("an error occurred during the request: %v", err)
+			t.Fatalf("an unexpected error occurred: %v", err)
 		}
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
