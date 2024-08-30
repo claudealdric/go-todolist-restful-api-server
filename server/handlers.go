ec61a803ca02723c62d26c32fc3ebb7e1c62138f
claudealdric <cvaldric@gmail.com>
2024-08-30 04:14:45 -0400

    Move the handlers to a separate file

---
+++ server/handlers.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/claudealdric/go-todolist-restful-api-server/models"
+)
+
+const jsonContentType = "application/json"
+
+func (s *Server) HandleDeleteTaskById(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(
+			w,
+			fmt.Sprintf("ID: %q is invalid", r.PathValue("id")),
+			http.StatusBadRequest,
+		)
+	}
+	s.store.DeleteTaskById(id)
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (s *Server) HandleGetTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	tasks, err := s.store.GetTasks()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(tasks); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *Server) HandlePostTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	var task models.Task
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	task, err = s.store.CreateTask(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(task); err != nil {
+		log.Printf("error encoding response: %v", err)
+	}
+}
+
+func (s *Server) HandleRoot(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
--- server/server.go
+++ server/server.go
@@ -1,17 +1,10 @@
 package server
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/claudealdric/go-todolist-restful-api-server/datastore"
-	"github.com/claudealdric/go-todolist-restful-api-server/models"
 )
-
-const jsonContentType = "application/json"
 
 type Server struct {
 	store datastore.DataStore
@@ -23,53 +16,4 @@ func NewServer(store datastore.DataStore) *Server {
 	router := NewRouter(server)
 	server.Handler = router
 	return server
-}
-
-func (s *Server) HandleDeleteTaskById(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {
-		http.Error(
-			w,
-			fmt.Sprintf("ID: %q is invalid", r.PathValue("id")),
-			http.StatusBadRequest,
-		)
-	}
-	s.store.DeleteTaskById(id)
-	w.WriteHeader(http.StatusNoContent)
-}
-
-func (s *Server) HandleGetTasks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
-	tasks, err := s.store.GetTasks()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if err := json.NewEncoder(w).Encode(tasks); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func (s *Server) HandlePostTasks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
-	var task models.Task
-	err := json.NewDecoder(r.Body).Decode(&task)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	task, err = s.store.CreateTask(task)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(task); err != nil {
-		log.Printf("error encoding response: %v", err)
-	}
-}
-
-func (s *Server) HandleRoot(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 }
