7b768427d51bdc9105045410bdd73a5941c4e293
claudealdric <cvaldric@gmail.com>
2024-08-30 16:01:08 -0400

    Write tests for the `ConvertToJSON` utility function

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -6,6 +6,39 @@ import (
 	"github.com/claudealdric/go-todolist-restful-api-server/testutils/assert"
 )
 
+func TestConvertToJSON(t *testing.T) {
+	t.Run("converts a struct to a JSON object", func(t *testing.T) {
+		type node struct {
+			Id int `json:"id"`
+		}
+		n := node{1}
+		json, err := ConvertToJSON(n)
+		want := `{"id":1}`
+
+		assert.NoError(t, err)
+		assert.Equals(t, string(json), want)
+	})
+
+	t.Run("converts a slice of structs to an array of JSON objects", func(t *testing.T) {
+		type node struct {
+			Id int `json:"id"`
+		}
+		s := []node{node{1}, node{2}}
+		json, err := ConvertToJSON(s)
+		want := `[{"id":1},{"id":2}]`
+
+		assert.NoError(t, err)
+		assert.Equals(t, string(json), want)
+	})
+
+	t.Run("returns a nil value and an error with an invalid JSON string", func(t *testing.T) {
+		invalidJsonInput := func() {}
+		json, err := ConvertToJSON(invalidJsonInput)
+		assert.HasError(t, err)
+		assert.Equals(t, json, nil)
+	})
+}
+
 func TestSliceFind(t *testing.T) {
 	t.Run("returns the searched element if it exists", func(t *testing.T) {
 		s := []int{1, 2, 3, 4, 5}
