8053afb42a5e88388a275a55e454106d80cce703
claudealdric <cvaldric@gmail.com>
2024-09-01 15:52:16 -0400

    Move the mock store to the `testutils` package

--- api/handlers_test.go
+++ api/handlers_test.go
@@ -3,23 +3,19 @@ package api
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"slices"
 	"testing"
 
-	"github.com/claudealdric/go-todolist-restful-api-server/data"
 	"github.com/claudealdric/go-todolist-restful-api-server/models"
 	"github.com/claudealdric/go-todolist-restful-api-server/testutils"
 	"github.com/claudealdric/go-todolist-restful-api-server/testutils/assert"
-	"github.com/claudealdric/go-todolist-restful-api-server/utils"
 )
 
 func TestHandleRoot(t *testing.T) {
 	t.Run("responds with 200 OK status", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -32,7 +28,7 @@ func TestHandleRoot(t *testing.T) {
 
 func TestHandleDeleteTaskById(t *testing.T) {
 	t.Run("deletes the task and responds with 204 No Content", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
 		tasks, err := data.GetTasks()
@@ -65,7 +61,7 @@ func TestHandleDeleteTaskById(t *testing.T) {
 	})
 
 	t.Run("responds with a 400 Bad Request when sending a non-integer ID", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
 		request := httptest.NewRequest(
@@ -80,7 +76,7 @@ func TestHandleDeleteTaskById(t *testing.T) {
 	})
 
 	t.Run("responds with 404 Not Found when the task does not exist", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
 		taskToDelete := models.Task{-1, "Does not exist"}
@@ -96,10 +92,10 @@ func TestHandleDeleteTaskById(t *testing.T) {
 	})
 
 	t.Run("responds with 500 error when the store task deletion fails for an unknown reason", func(t *testing.T) {
-		data := newMockStore(true)
+		data := testutils.NewMockStore(true)
 		server := NewServer(data)
 
-		taskToDelete := initialTasks[0]
+		taskToDelete := testutils.InitialMockStoreTasks[0]
 		request := httptest.NewRequest(
 			http.MethodDelete,
 			fmt.Sprintf("/tasks/%d", taskToDelete.Id),
@@ -114,10 +110,10 @@ func TestHandleDeleteTaskById(t *testing.T) {
 
 func TestHandleGetTaskById(t *testing.T) {
 	t.Run("returns the wanted task if it exists", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
-		wantedTask := initialTasks[0]
+		wantedTask := testutils.InitialMockStoreTasks[0]
 		request := httptest.NewRequest(
 			http.MethodGet,
 			fmt.Sprintf("/tasks/%d", wantedTask.Id),
@@ -132,7 +128,7 @@ func TestHandleGetTaskById(t *testing.T) {
 			jsonContentType,
 		)
 		assert.Status(t, response.Code, http.StatusOK)
-		assert.Calls(t, data.getTaskByIdCalls, 1)
+		assert.Calls(t, data.GetTaskByIdCalls, 1)
 		assert.Equals(
 			t,
 			testutils.GetTaskFromResponse(t, response.Body),
@@ -141,7 +137,7 @@ func TestHandleGetTaskById(t *testing.T) {
 	})
 
 	t.Run("responds with a 400 Bad Request when given non-integer ID", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
 		invalidId := "not-an-integer"
@@ -154,11 +150,11 @@ func TestHandleGetTaskById(t *testing.T) {
 		server.Handler.ServeHTTP(response, request)
 
 		assert.Status(t, response.Code, http.StatusBadRequest)
-		assert.Calls(t, data.getTaskByIdCalls, 0)
+		assert.Calls(t, data.GetTaskByIdCalls, 0)
 	})
 
 	t.Run("responds with a 404 Not Found when the task cannot be found", func(t *testing.T) {
-		data := newMockStore(true)
+		data := testutils.NewMockStore(true)
 		server := NewServer(data)
 
 		doesNotExistId := -1
@@ -171,11 +167,11 @@ func TestHandleGetTaskById(t *testing.T) {
 		server.Handler.ServeHTTP(response, request)
 
 		assert.Status(t, response.Code, http.StatusNotFound)
-		assert.Calls(t, data.getTaskByIdCalls, 1)
+		assert.Calls(t, data.GetTaskByIdCalls, 1)
 	})
 
 	t.Run("responds with a 500 error when the task retrieval fails for an unknown reason", func(t *testing.T) {
-		data := newMockStore(true)
+		data := testutils.NewMockStore(true)
 		server := NewServer(data)
 
 		doesNotExistId := -2
@@ -188,13 +184,13 @@ func TestHandleGetTaskById(t *testing.T) {
 		server.Handler.ServeHTTP(response, request)
 
 		assert.Status(t, response.Code, http.StatusInternalServerError)
-		assert.Calls(t, data.getTaskByIdCalls, 1)
+		assert.Calls(t, data.GetTaskByIdCalls, 1)
 	})
 }
 
 func TestHandleGetTasks(t *testing.T) {
 	t.Run("returns the stored tasks", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
 		request := httptest.NewRequest(http.MethodGet, "/tasks", nil)
@@ -207,16 +203,16 @@ func TestHandleGetTasks(t *testing.T) {
 			jsonContentType,
 		)
 		assert.Status(t, response.Code, http.StatusOK)
-		assert.Calls(t, data.getTasksCalls, 1)
+		assert.Calls(t, data.GetTasksCalls, 1)
 		assert.Equals(
 			t,
 			testutils.GetTasksFromResponse(t, response.Body),
-			initialTasks,
+			testutils.InitialMockStoreTasks,
 		)
 	})
 
 	t.Run("responds with a 500 error when getting tasks from the store errors", func(t *testing.T) {
-		data := newMockStore(true)
+		data := testutils.NewMockStore(true)
 		server := NewServer(data)
 
 		request := httptest.NewRequest(http.MethodGet, "/tasks", nil)
@@ -224,13 +220,13 @@ func TestHandleGetTasks(t *testing.T) {
 		server.Handler.ServeHTTP(response, request)
 
 		assert.Status(t, response.Code, http.StatusInternalServerError)
-		assert.Calls(t, data.getTasksCalls, 1)
+		assert.Calls(t, data.GetTasksCalls, 1)
 	})
 }
 
 func TestHandlePostTasks(t *testing.T) {
 	t.Run("creates and returns the task with a 201 Status Created", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
 		newTask := models.Task{2, "Exercise"}
@@ -250,7 +246,7 @@ func TestHandlePostTasks(t *testing.T) {
 			jsonContentType,
 		)
 		assert.Status(t, response.Code, http.StatusCreated)
-		assert.Calls(t, data.createTaskCalls, 1)
+		assert.Calls(t, data.CreateTaskCalls, 1)
 		assert.Equals(
 			t,
 			testutils.GetTaskFromResponse(t, response.Body),
@@ -259,7 +255,7 @@ func TestHandlePostTasks(t *testing.T) {
 	})
 
 	t.Run("responds with a 400 Bad Request given an invalid body", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
 		invalidJson := `{`
@@ -272,11 +268,11 @@ func TestHandlePostTasks(t *testing.T) {
 		server.Handler.ServeHTTP(response, request)
 
 		assert.Status(t, response.Code, http.StatusBadRequest)
-		assert.Calls(t, data.createTaskCalls, 0)
+		assert.Calls(t, data.CreateTaskCalls, 0)
 	})
 
 	t.Run("responds with a 500 error when the store task creation fails", func(t *testing.T) {
-		data := newMockStore(true)
+		data := testutils.NewMockStore(true)
 		server := NewServer(data)
 
 		newTask := models.Task{2, "Exercise"}
@@ -291,16 +287,16 @@ func TestHandlePostTasks(t *testing.T) {
 		server.Handler.ServeHTTP(response, request)
 
 		assert.Status(t, response.Code, http.StatusInternalServerError)
-		assert.Calls(t, data.createTaskCalls, 1)
+		assert.Calls(t, data.CreateTaskCalls, 1)
 	})
 }
 
 func TestHandlePatchTasks(t *testing.T) {
 	t.Run("returns the updated task and responds with a 200 OK status", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
-		task := initialTasks[0]
+		task := testutils.InitialMockStoreTasks[0]
 
 		newTitle := "Pack bags"
 		dto := models.UpdateTaskDTO{Title: &newTitle}
@@ -321,7 +317,7 @@ func TestHandlePatchTasks(t *testing.T) {
 			jsonContentType,
 		)
 		assert.Status(t, response.Code, http.StatusOK)
-		assert.Calls(t, data.updateTaskCalls, 1)
+		assert.Calls(t, data.UpdateTaskCalls, 1)
 		assert.Equals(
 			t,
 			testutils.GetTaskFromResponse(t, response.Body),
@@ -330,7 +326,7 @@ func TestHandlePatchTasks(t *testing.T) {
 	})
 
 	t.Run("responds with a 400 Bad Request with an invalid ID", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
 		invalidId := "not-an-integer"
@@ -348,11 +344,11 @@ func TestHandlePatchTasks(t *testing.T) {
 		server.Handler.ServeHTTP(response, request)
 
 		assert.Status(t, response.Code, http.StatusBadRequest)
-		assert.Calls(t, data.updateTaskCalls, 0)
+		assert.Calls(t, data.UpdateTaskCalls, 0)
 	})
 
 	t.Run("responds with a 404 Not Found when the task cannot be found", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
 		doesNotExistId := -1
@@ -370,14 +366,14 @@ func TestHandlePatchTasks(t *testing.T) {
 		server.Handler.ServeHTTP(response, request)
 
 		assert.Status(t, response.Code, http.StatusNotFound)
-		assert.Calls(t, data.updateTaskCalls, 1)
+		assert.Calls(t, data.UpdateTaskCalls, 1)
 	})
 
 	t.Run("responds with a 400 Bad Request when the body is invalid", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 
-		task := initialTasks[0]
+		task := testutils.InitialMockStoreTasks[0]
 
 		request := httptest.NewRequest(
 			http.MethodPatch,
@@ -388,14 +384,14 @@ func TestHandlePatchTasks(t *testing.T) {
 		server.Handler.ServeHTTP(response, request)
 
 		assert.Status(t, response.Code, http.StatusBadRequest)
-		assert.Calls(t, data.updateTaskCalls, 0)
+		assert.Calls(t, data.UpdateTaskCalls, 0)
 	})
 
 	t.Run("responds with a 500 error when an unknown store error occurs", func(t *testing.T) {
-		data := newMockStore(true)
+		data := testutils.NewMockStore(true)
 		server := NewServer(data)
 
-		task := initialTasks[0]
+		task := testutils.InitialMockStoreTasks[0]
 
 		newTitle := "Pack bags"
 		dto := models.UpdateTaskDTO{Title: &newTitle}
@@ -411,16 +407,16 @@ func TestHandlePatchTasks(t *testing.T) {
 		server.Handler.ServeHTTP(response, request)
 
 		assert.Status(t, response.Code, http.StatusInternalServerError)
-		assert.Calls(t, data.updateTaskCalls, 1)
+		assert.Calls(t, data.UpdateTaskCalls, 1)
 	})
 
 	t.Run("providing the ID in the request body does not override the ID URL param", func(t *testing.T) {
-		data := newMockStore(false)
+		data := testutils.NewMockStore(false)
 		server := NewServer(data)
 		unmodifiedTask := models.Task{Id: 2, Title: "Exercise"}
 		server.store.CreateTask(unmodifiedTask)
 
-		taskToUpdate := initialTasks[0]
+		taskToUpdate := testutils.InitialMockStoreTasks[0]
 
 		newTitle := "Pack bags"
 		jsonData, err := json.Marshal(models.Task{Id: 2, Title: newTitle})
@@ -440,7 +436,7 @@ func TestHandlePatchTasks(t *testing.T) {
 			jsonContentType,
 		)
 		assert.Status(t, response.Code, http.StatusOK)
-		assert.Calls(t, data.updateTaskCalls, 1)
+		assert.Calls(t, data.UpdateTaskCalls, 1)
 		assert.Equals(
 			t,
 			testutils.GetTaskFromResponse(t, response.Body),
@@ -450,85 +446,4 @@ func TestHandlePatchTasks(t *testing.T) {
 		gotUnmodifiedTask, _ := server.store.GetTaskById(unmodifiedTask.Id)
 		assert.Equals(t, gotUnmodifiedTask, unmodifiedTask)
 	})
-}
-
-var initialTasks = []models.Task{{1, "Pack clothes"}}
-var forcedError = errors.New("forced error")
-
-type mockStore struct {
-	createTaskCalls  int
-	getTaskByIdCalls int
-	getTasksCalls    int
-	updateTaskCalls  int
-	tasks            []models.Task
-	shouldError      bool
-}
-
-func newMockStore(shouldError bool) *mockStore {
-	m := &mockStore{tasks: initialTasks, shouldError: shouldError}
-	return m
-}
-
-func (m *mockStore) CreateTask(task models.Task) (models.Task, error) {
-	m.createTaskCalls++
-	if m.shouldError {
-		return models.Task{}, forcedError
-	}
-	m.tasks = append(m.tasks, task)
-	return task, nil
-}
-
-func (m *mockStore) GetTaskById(id int) (models.Task, error) {
-	m.getTaskByIdCalls++
-	var task models.Task
-	if m.shouldError {
-		if id == -1 {
-			return task, data.ErrResourceNotFound
-		} else {
-			return task, forcedError
-		}
-	}
-	tasks, _ := m.GetTasks()
-	task, _ = utils.SliceFind(tasks, func(t models.Task) bool {
-		return t.Id == id
-	})
-	return task, nil
-}
-
-func (m *mockStore) GetTasks() ([]models.Task, error) {
-	m.getTasksCalls++
-	if m.shouldError {
-		return nil, forcedError
-	}
-	return m.tasks, nil
-}
-
-func (m *mockStore) DeleteTaskById(id int) error {
-	if m.shouldError {
-		return forcedError
-	}
-	i := slices.IndexFunc(m.tasks, func(task models.Task) bool {
-		return task.Id == id
-	})
-	if i == -1 {
-		return data.ErrResourceNotFound
-	}
-	m.tasks = slices.DeleteFunc(m.tasks, func(task models.Task) bool {
-		return task.Id == id
-	})
-	return nil
-}
-
-func (m *mockStore) UpdateTask(task models.Task) (models.Task, error) {
-	m.updateTaskCalls++
-	if m.shouldError {
-		return models.Task{}, forcedError
-	}
-	for i, t := range m.tasks {
-		if t.Id == task.Id {
-			m.tasks[i] = task
-			return task, nil
-		}
-	}
-	return models.Task{}, data.ErrResourceNotFound
 }
---
+++ testutils/mock_store.go
@@ -0,0 +1,91 @@
+package testutils
+
+import (
+	"errors"
+	"slices"
+
+	"github.com/claudealdric/go-todolist-restful-api-server/data"
+	"github.com/claudealdric/go-todolist-restful-api-server/models"
+	"github.com/claudealdric/go-todolist-restful-api-server/utils"
+)
+
+var InitialMockStoreTasks = []models.Task{{1, "Pack clothes"}}
+var forcedError = errors.New("forced error")
+
+type mockStore struct {
+	CreateTaskCalls  int
+	GetTaskByIdCalls int
+	GetTasksCalls    int
+	UpdateTaskCalls  int
+	tasks            []models.Task
+	shouldError      bool
+}
+
+func NewMockStore(shouldError bool) *mockStore {
+	m := &mockStore{tasks: InitialMockStoreTasks, shouldError: shouldError}
+	return m
+}
+
+func (m *mockStore) CreateTask(task models.Task) (models.Task, error) {
+	m.CreateTaskCalls++
+	if m.shouldError {
+		return models.Task{}, forcedError
+	}
+	m.tasks = append(m.tasks, task)
+	return task, nil
+}
+
+func (m *mockStore) GetTaskById(id int) (models.Task, error) {
+	m.GetTaskByIdCalls++
+	var task models.Task
+	if m.shouldError {
+		if id == -1 {
+			return task, data.ErrResourceNotFound
+		} else {
+			return task, forcedError
+		}
+	}
+	tasks, _ := m.GetTasks()
+	task, _ = utils.SliceFind(tasks, func(t models.Task) bool {
+		return t.Id == id
+	})
+	return task, nil
+}
+
+func (m *mockStore) GetTasks() ([]models.Task, error) {
+	m.GetTasksCalls++
+	if m.shouldError {
+		return nil, forcedError
+	}
+	return m.tasks, nil
+}
+
+func (m *mockStore) DeleteTaskById(id int) error {
+	if m.shouldError {
+		return forcedError
+	}
+	i := slices.IndexFunc(m.tasks, func(task models.Task) bool {
+		return task.Id == id
+	})
+	if i == -1 {
+		return data.ErrResourceNotFound
+	}
+	m.tasks = slices.DeleteFunc(m.tasks, func(task models.Task) bool {
+		return task.Id == id
+	})
+	return nil
+}
+
+func (m *mockStore) UpdateTask(task models.Task) (models.Task, error) {
+	m.UpdateTaskCalls++
+	if m.shouldError {
+		return models.Task{}, forcedError
+	}
+	for i, t := range m.tasks {
+		if t.Id == task.Id {
+			m.tasks[i] = task
+			return task, nil
+		}
+	}
+	return models.Task{}, data.ErrResourceNotFound
+}
