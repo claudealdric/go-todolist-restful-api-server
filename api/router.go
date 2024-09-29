443958b658508f64af2169ede89ce3b4b7a80679
claudealdric <cvaldric@gmail.com>
2024-09-29 15:24:02 -0400

    Refactor the router

--- api/router.go
+++ api/router.go
@@ -5,18 +5,41 @@ import (
 	"net/http"
 )
 
-func NewRouter(s *Server) *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc(get("/{$}"), s.HandleRoot)
-	mux.HandleFunc(get("/tasks"), s.HandleGetTasks)
-	mux.HandleFunc(get("/tasks/{id}"), s.HandleGetTaskById)
-	mux.HandleFunc("PATCH /tasks/{id}", s.HandlePatchTask)
-	mux.HandleFunc("POST /tasks", s.HandlePostTask)
-	mux.HandleFunc("POST /users", s.HandlePostUser)
-	mux.HandleFunc("DELETE /tasks/{id}", s.HandleDeleteTask)
-	return mux
+type Router struct {
+	http.ServeMux
 }
 
-func get(path string) string {
-	return fmt.Sprintf("%s %s", http.MethodGet, path)
+func NewRouter(s *Server) *Router {
+	r := Router{}
+	r.Get("/{$}", s.HandleRoot)
+	r.Get("/tasks", s.HandleGetTasks)
+	r.Get("/tasks/{id}", s.HandleGetTaskById)
+	r.Patch("/tasks/{id}", s.HandlePatchTask)
+	r.Post("/tasks", s.HandlePostTask)
+	r.Post("/users", s.HandlePostUser)
+	r.Delete("/tasks/{id}", s.HandleDeleteTask)
+	return &r
+}
+
+func (r *Router) Delete(pattern string, handlerFunc http.HandlerFunc) {
+	r.getHandlerFuncPattern(http.MethodDelete, pattern, handlerFunc)
+}
+
+func (r *Router) Get(pattern string, handlerFunc http.HandlerFunc) {
+	r.getHandlerFuncPattern(http.MethodGet, pattern, handlerFunc)
+}
+
+func (r *Router) Patch(pattern string, handlerFunc http.HandlerFunc) {
+	r.getHandlerFuncPattern(http.MethodPatch, pattern, handlerFunc)
+}
+
+func (r *Router) Post(pattern string, handlerFunc http.HandlerFunc) {
+	r.getHandlerFuncPattern(http.MethodPost, pattern, handlerFunc)
+}
+
+func (r *Router) getHandlerFuncPattern(
+	method, pattern string,
+	handlerFunc http.HandlerFunc,
+) {
+	r.HandleFunc(fmt.Sprintf("%s %s", method, pattern), handlerFunc)
 }
